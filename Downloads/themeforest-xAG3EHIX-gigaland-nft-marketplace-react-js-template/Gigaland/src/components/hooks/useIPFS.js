d52b7fcb7f3f5f3c60566d08fcf493127c425a79
NyftieDAO <98790380+NyftieDAO@users.noreply.github.com>
2022-01-31 22:48:02 -0500

    Delete Downloads/themeforest-xAG3EHIX-gigaland-nft-marketplace-react-js-template/Gigaland directory

--- Downloads/themeforest-xAG3EHIX-gigaland-nft-marketplace-react-js-template/Gigaland/src/components/hooks/useIPFS.js
+++
@@ -1,9 +0,0 @@
-export const useIPFS = () => {
-    const resolveLink = (url) => {
-      if (!url.includes("ipfs://") || !url) return url;
-      return url.replace("ipfs://", "https://gateway.ipfs.io/ipfs/");
-    };
-  
-    return { resolveLink };
-  };
-  
